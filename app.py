be2a793c416a2311be6d355063b49a22c27ce3cf
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-10-29 14:08:23 +0330

    create database with flask-sqlalchemy

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from blueprints.general import app as general
 from blueprints.user import app as user
 from blueprints.admin import app as admin
 
+# create sqlalchemy object as db
+db = SQLAlchemy()
 
+# create the app
 app = Flask(__name__)
+
+# register blueprints
 app.register_blueprint(general)
 app.register_blueprint(user)
 app.register_blueprint(admin)
 
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+# initialize the app with the extension
+db.init_app(app)
 
+# create database
+with app.app_context():
+    db.create_all()
+
+# runserver
 if __name__ == '__main__':
     app.run()
